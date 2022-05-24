6d33dceaaa099552c38d7602ab48e9061dd18831
Giovanni Almeida <69661645+GiovanniBot@users.noreply.github.com>
2022-05-24 15:29:23 -0300

    chore: update modal file & add main file

---
+++ scripts/main.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+    modalListener();
+    deleteTaskListener();
+    addEventListeners();
+});
--- scripts/modal.js
+++ scripts/modal.js
@@ -1,28 +1,21 @@
-$(document).ready(function() {
-    var modalBtn = $('.modal-btn');
-    var modalBg = $('.modal-bg');
-    var modalClose = $('.modal-close');
-    var input = $('#taskNameInput');
-    var createTaskBtn = $('.create-task-btn');
+let openModalBtn, modalBg,  input, createTaskBtn, modalCloseBtn;
+openModalBtn = $('.modal-btn');
+modalBg = $('.modal-bg');
+input = modalBg.find('input:first');
+createTaskBtn = modalBg.find('button:first');
+modalCloseBtn = modalBg.find('span:last');
 
-    modalBtn.on('click', function() {
+function modalListener() {
+    openModalBtn.click(function() {
         modalBg.addClass('modal-active');
     });
-    modalClose.on('click', function() {
+    modalCloseBtn.click(function() {
         modalBg.removeClass('modal-active');
     });
-    createTaskBtn.on('click', function() {
-        if(input.val().length >= 1) {
-            modalBg.removeClass('modal-active');
-        } else {
-            alert('Please insert the name of your new task.');
-        }
+    createTaskBtn.click(function() {
+        input.val().length > 0 ? modalBg.removeClass('modal-active') : alert('Please insert the name of your new task.');
     });
-    modalBg.on('click', function(event) {
-        if(event.target.className === "modal-bg modal-active") {
-            modalBg.removeClass('modal-active');
-        }
+    modalBg.click(function(event) {
+        event.target.className == "modal-bg modal-active" ? $(this).removeClass('modal-active') : $(this);
     });
-    deleteTaskListener();
-    addEventListeners();
-});
+}
